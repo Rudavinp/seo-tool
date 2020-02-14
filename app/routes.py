e500f14af9d71561076e1c2896e210465f4ea3e2
R <rudavinp@gmail.com>
2020-02-14 15:34:54 +0300

    add render text and logic pars text

--- app/routes.py
+++ app/routes.py
@@ -2,15 +2,44 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import InputTextForm
 from google_app import yandex
+from markupsafe import Markup
+import time
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = InputTextForm()
-    if form.validate_on_submit():
-        flash('Your text is being processed - {}'.format(form.text.data))
-        print(1, type(form.text.data))
-        print(2, len(form.text.data))
-        yandex(form.text.data)
+    text_ = "Text"
+    text = Markup('<span style="color: #fa8e47">{}</span>'.format(text_))
+    # text = Markup('<p>{}</p>'.format(str('<span style="color: #fa8e47">У людей было много видов, до наших дней дожил только один – современный человек разумный</span>.<span style="color: #fa8e47"> Много ветвей человечества оказались тупиковыми и вымерли в результате природных катаклизмов, войн и эволюции</span>.<span style="color: #fa8e47"> О восьми представителях людского рода, ведущих свою историю 2,8 млн лет – в нашем материале</span>.<span style="color: #fa8e47"></span>')))
 
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form)
+    if form.validate_on_submit():
+        list_to_template = []
+        list_matches = {}
+        flash('Your text is being processed - {}'.format(form.text.data))
+        list_sentences = form.text.data.split('.')
+
+        for sen in list_sentences:
+            time.sleep(2)
+            result = yandex(sen)
+            print(1, result)
+            if result:
+                list_matches[sen] = result
+            else:
+                list_matches[sen] = []
+
+        for k, v in list_matches.items():
+            if list_matches.get(k):
+                list_to_template.append(Markup('<span style="color: #FF6347">{}</span>'.format(k)))
+            else:
+                list_to_template.append(Markup('<span style="color: #00FF00">{}</span>'.format(k)))
+
+        text = '.'.join(list_to_template)
+        text = Markup('<p>{}</p>'.format(text))
+        print(12, list_sentences)
+        print(12, text)
+        # snippets = yandex(form.text.data)
+
+
+        # return redirect(url_for('index', text=text))
+        return render_template('index.html', form=form, text=text)
+    return render_template('index.html', form=form, text=text)
--- google_app.py
+++ google_app.py
@@ -4,6 +4,7 @@ from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
 
 USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)'
+# USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
 
 query = '"Я люблю айфон"'
 q = quote_plus(query)
@@ -12,6 +13,7 @@ page = "https://www.google.ru/"
 search_yandex = 'https://www.yandex.ru/search/?lr=213&text={}'
 
 def get_page(query, w=True):
+    query = '"' + query + '"'
     query = quote_plus(query)
     page = search_yandex.format(query)
     print(1, page)
@@ -27,31 +29,58 @@ def get_page(query, w=True):
     return html
 
 def yandex(query):
+    snippets_links = {}
     list_snippets = []
+    list_links = []
     snippets_yes = []
     snippets_not = []
 
     html = get_page(query)
     soup = BeautifulSoup(html, 'html.parser')
-    snippets = soup.findAll('div', class_='serp-item__text')
-    links = soup.find_all('a')
-    links_ = soup.find_all('a', class_='serp-item__title-link')
-    print(22, links_)
-    for _ in links_:
-        print(55, _.get('href'))
+
+    # snippets = soup.findAll('div', class_='serp-item__text')
+    # print(soup.prettify())
+    blocks = soup.find_all('div', class_='serp-item')
+    print(34, type(blocks))
+    for b in blocks:
+        print(type(b))
+        snip = b.find('div', class_='serp-item__text')
+        link = b.find('a', class_='serp-item__title-link')
+        if snip and query.lower() in snip.text.lower():
+            list_snippets.append(snip.text.lower())
+        else:
+            list_snippets.append('')
+        if link:
+            list_links.append(link.get('href'))
+        else:
+            list_links.append('')
+
+
+
+    # links = soup.find_all('a')
+    # print(33, links)
+    # links_ = soup.find_all('a', class_='serp-item__title-link')
+    # print(len(snippets))
+    # print(len(links_))
+    # for _ in links_:
+    #     print(55, _.get('href'))
     # for l in links:
     #     print(33, l.get('class'))
     #     print(44, l.get('href'))
-    list_links = [l.text.lower() for l in links]
-    for s in snippets:
-        list_snippets.append(s.text.lower())
-
+    # list_links = [l.text.lower() for l in links if ]
+    # for s in snippets:
+    #     list_snippets.append(s.text.lower())
+    #
     for s in list_snippets:
         if query.lower() in s:
             list_snippets[list_snippets.index(s)] = list_snippets[list_snippets.index(s)] + '_yes'
         else:
             list_snippets[list_snippets.index(s)]  = list_snippets[list_snippets.index(s)]  + '_not'
-    for i in list_snippets:
-        print(i)
+
+    snippets_links = zip(list_snippets, list_links)
+    # print(tuple(snippets_links))
+    return list_links
+    # for i in list_snippets:
+    #     print(i)
     # print(list_links)
 
