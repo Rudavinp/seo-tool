1a7b964ece3b2cd133cbfa7c4c348773db4f1dec
R <rudavinp@gmail.com>
2020-02-19 16:56:56 +0300

    fix

--- google_app.py
+++ google_app.py
@@ -58,7 +58,8 @@ def get_page_with_selenium(query):
     opts = Options()
     opts.set_headless()
     binary = FirefoxBinary('/app/vendor/firefox/firefox')
-    driver = Firefox(options=opts, firefox_binary=binary, executable_path=r'/app/vendor/geckodriver/geckodriver')
+    # driver = Firefox(options=opts, firefox_binary=binary, executable_path=r'/app/vendor/geckodriver/geckodriver')
+    driver = Firefox(options=opts, firefox_binary=binary)
     driver.wait = WebDriverWait(driver, 5)
     driver.get('https://www.yandex.ru')
     # try:
