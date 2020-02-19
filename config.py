49a57237795db1261974c08d03b16e20213eff65
R <rudavinp@gmail.com>
2020-02-19 16:13:52 +0300

    add bebug mode

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'lol'
+    FLASK_ENV = 'development'
     # PATH = "/usr/local/bin:/usr/bin:/bin:/app/vendor/"
     # LD_LIBRARY_PATH = "/usr/local/lib:/usr/lib:/lib:/app/vendor"
